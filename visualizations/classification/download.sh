ba3a57411ddee4186ddad2f6494e6ed34a67878f
Michael Hasey <50347182+michaelhasey@users.noreply.github.com>
2021-08-28 02:01:56 -0400

    Delete visualizations/classification directory

--- visualizations/classification/download.sh
+++
@@ -1,3 +0,0 @@
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
-unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
-rm shapenetcore_partanno_segmentation_benchmark_v0.zip
